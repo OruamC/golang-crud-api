842b7df08d47f6dee3bca38f0a720b9daaa1fa4e
Oruam Catani <oru_catani@hotmail.com>
2025-02-20 16:24:05 -0300

    feat: create user request and response

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,11 +1,22 @@
 package controller
 
 import (
+	"fmt"
 	"github.com/OruamC/golang-crud-api/src/configuration/rest_err"
+	"github.com/OruamC/golang-crud-api/src/controller/model/request"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context) {
-	err := rest_err.NewBadRequestError("Voce chamou a rota de forma errada")
-	c.JSON(err.Code, err)
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(
+			fmt.Sprintf("There are some incorrect fields, error=%s", err.Error()))
+
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+
+	fmt.Println(userRequest)
 }
