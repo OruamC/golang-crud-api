fb10045ded88e1f0bf3f06b810e29bb4632911c3
Oruam Catani <oru_catani@hotmail.com>
2025-02-25 09:40:01 -0300

    feat: user validation fields

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -2,21 +2,32 @@ package controller
 
 import (
 	"fmt"
-	"github.com/OruamC/golang-crud-api/src/configuration/rest_err"
+	"github.com/OruamC/golang-crud-api/src/configuration/validation"
 	"github.com/OruamC/golang-crud-api/src/controller/model/request"
+	"github.com/OruamC/golang-crud-api/src/controller/model/response"
 	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
 )
 
 func CreateUser(c *gin.Context) {
+	log.Println("Init createUser controller")
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
-		restErr := rest_err.NewBadRequestError(
-			fmt.Sprintf("Some incorrect fields, error=%s", err.Error()))
+		log.Printf("Error trying to marshal object, error: %s", err.Error())
+		restErr := validation.ValidateUserError(err)
 
 		c.JSON(restErr.Code, restErr)
 		return
 	}
 
 	fmt.Println(userRequest)
+	responseUser := response.UserResponse{
+		ID:    "1",
+		Email: userRequest.Email,
+		Name:  userRequest.Name,
+		Age:   userRequest.Age,
+	}
+	c.JSON(http.StatusCreated, responseUser)
 }
